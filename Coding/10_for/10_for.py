bb33cd8a1ae7a6c651414b203a8bf66f615f60f4
Kimginam97 <rlarlska79@naver.com>
2021-05-12 14:57:02 +0900

    11주차 정리

---
+++ Coding/10_for/10_for.py
@@ -0,0 +1,21 @@
+# a = [(1, 2), (3, 4)]
+#
+# for i, j in a:
+#     print(j)
+
+
+# a = [1, 2, 3]
+# i = 0
+#
+# while i < len(a):
+#     print(a[i])
+#     i += 1
+
+
+t = ("a", "b", "c", "d", "e")
+s = ""
+
+for c in t:
+    s += c
+else:
+    print(s)
